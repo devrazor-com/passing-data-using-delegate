65c7acdb5141e24b4bee38fdb0a59d5430de26d7
Victor Balas <vbalas1@rogers.com>
2020-06-27 10:20:03 -0400

    Initial Commit

---
+++ passing-data-using-delegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  passing-data-using-delegate
+//
+//  Created by Victor on 2020-06-27.
+//  Copyright © 2020 Victor Balas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
